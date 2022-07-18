125abfb39d21e7a02876a55dc909235a12656819
weedakii <weeds0250@gmail.com>
2022-07-18 20:55:16 +0200

    h

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config({path: 'backend/config/config.env'})
+if(process.env.NODE_ENV !== 'PRODUCTION'){
+    dotenv.config({path: 'backend/config/config.env'})
+}
 
 const connectdb = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
